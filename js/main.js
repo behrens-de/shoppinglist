486d3ebcd0c729ca187ec2ef43f4aeae25b15805
Jan Behrens <dev@jpbehrens.de>
2021-11-21 21:13:26 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 // JP BHERENS - SHOPPING LIST
 
-
-
-
-const myList = [];
+const myList = ['TEST 1', 'TEST 2'];
 
 function addItem() {
     const form = document.querySelector('.addItem');
@@ -12,12 +9,20 @@ function addItem() {
     form.addEventListener('submit', (e) => {
         e.preventDefault();
         const value = input.value;
-        //alert(value);
-        myList.push(value);
-        loadList();
+        const valid = validateItem(input.value);
 
-        input.value = '';
+        input.value = valid ? '' : value;
     });
+}
+
+
+function validateItem(input) {
+    if (input.length > 2) {
+        myList.push(input);
+        loadList();
+        return true;
+    }
+    return false;
 }
 
 
@@ -27,16 +32,43 @@ function loadList() {
     itmes.innerHTML = '';
 
     myList.forEach((item) => {
-        itmes.innerHTML += `<li>${item}</li>`;
+        const li = document.createElement('li');
+        li.classList.add('test');
+        li.innerHTML = item;
+
+        itmes.appendChild(li);
+        //itmes.innerHTML += `<li class="test">${item}</li>`;
     });
+    countItems();
+    selectItem();
 }
 
 
+function selectItem() {
+    const items = document.querySelectorAll('.test');
+    items.forEach((item) => {
+        item.addEventListener('click', clickItem);
+    })
+}
+
+function clickItem() {
+    // Toggle (.active)
+    const activeClassName = "active";
+    this.classList.contains(activeClassName) ? this.classList.remove(activeClassName) : this.classList.add(activeClassName);
+}
+
+
+function countItems() {
+    const quanty = myList.length;
+    const headline = document.querySelector('.countItems');
+    headline.innerHTML = `${quanty} Einträge`;
+}
 
 
 function init() {
     addItem();
     loadList();
+
 }
 
 init();
