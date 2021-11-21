2aead214c7692ae82c4cd2afd97177044a6925b3
Jan Behrens <dev@jpbehrens.de>
2021-11-21 21:27:39 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
 // JP BHERENS - SHOPPING LIST
 
+
+let lists = [
+    {
+        id: 'jp-default',
+        name: 'Allgemein'
+    }
+];
+
+
+let item = {
+    list: 'jp-default', // id of the List
+    done: true,
+    name: 'Example'
+}
+
 const myList = ['TEST 1', 'TEST 2'];
 
+// Added the Items to the Array - myList
 function addItem() {
     const form = document.querySelector('.addItem');
     const input = form.querySelector('input');
@@ -10,12 +26,11 @@ function addItem() {
         e.preventDefault();
         const value = input.value;
         const valid = validateItem(input.value);
-
         input.value = valid ? '' : value;
     });
 }
 
-
+// Ckecked if the addItem is valid
 function validateItem(input) {
     if (input.length > 2) {
         myList.push(input);
@@ -26,24 +41,23 @@ function validateItem(input) {
 }
 
 
+// Display the List
 function loadList() {
-
     const itmes = document.querySelector('.items ul');
     itmes.innerHTML = '';
 
     myList.forEach((item) => {
+        // Creates Items  
         const li = document.createElement('li');
         li.classList.add('test');
         li.innerHTML = item;
-
         itmes.appendChild(li);
-        //itmes.innerHTML += `<li class="test">${item}</li>`;
     });
     countItems();
     selectItem();
 }
 
-
+// If item is done
 function selectItem() {
     const items = document.querySelectorAll('.test');
     items.forEach((item) => {
@@ -57,7 +71,7 @@ function clickItem() {
     this.classList.contains(activeClassName) ? this.classList.remove(activeClassName) : this.classList.add(activeClassName);
 }
 
-
+// Count the Items
 function countItems() {
     const quanty = myList.length;
     const headline = document.querySelector('.countItems');
