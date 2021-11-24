d2893e97cd13ee30cf2c033e90b043298a3334d2
Jan Behrens <dev@jpbehrens.de>
2021-11-24 18:55:04 +0100

    update

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 
 class App {
+
+    _maxLists = 10;
+    // Erstellt eine ID
     generateID(format = 'xxxx-xxxx-xxxx-xxxx') {
         var d = new Date().getTime();
         var d2 = (performance && performance.now && (performance.now() * 1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
@@ -18,17 +21,69 @@ class App {
     };
 
 
+    // Erstellt eine neue Liste
     addList(user, list) {
         const form = document.querySelector('.addList');
         const input = form.querySelector('input');
+
+        const getList = JSON.parse(list.get()) ?? [];
+
         form.addEventListener('submit', (event) => {
             event.preventDefault();
-            list.set({
-                name: input.value,
-                id: user.getId() + this.generateID('xxxxx')
-            });
-            input.value = '';
+
+            const val = input.value;
+
+            if (list.exist(val) || val.length < 2 || getList.length >= this._maxLists) {
+                
+                if(list.exist(val)){
+                    this.dialog(`Eine Liste mit dem Namen ${val} exestiert schon`);
+                }
+                if(val.length < 2){
+                    this.dialog(`Eingabe ist zu Kurz`);
+                }
+
+                if(getList.length >= this._maxLists){
+                    this.dialog(`Sie können maximal ${this._maxLists} Listen anlegen`);
+                }
+                
+            } else {
+                list.set({
+                    name: input.value,
+                    id: user.getId() + this.generateID('xxxxx')
+                });
+                input.value = '';
+                this.renderList(list);
+            }
         });
+    }
+
+    renderList(list){
+        const target = document.querySelector('.myLists');
+        target.innerHTML = null;
+        const getList = JSON.parse(list.get()) ?? [];
+
+        if(getList.length < 1){
+            const div = document.createElement('div');
+            div.classList.add('nolists');
+            div.innerHTML = 'Noch keine Liste vorhanden';
+            target.appendChild(div);
+            return;
+        }
+
+        getList.forEach((liste)=>{
+            const name = liste.name;
+            const div = document.createElement('div');
+            div.classList.add('list');
+            div.innerHTML = name;
+
+            target.appendChild(div);
+
+        });
+
+    }
+
+    dialog(msg){
+        alert(msg);
     }
 }
 
@@ -70,6 +125,17 @@ class List {
     get() {
         return localStorage.getItem(this._storageKey);
     }
+
+    exist(name) {
+        const getList = JSON.parse(this.get()) ?? [];
+        let exist = false;
+        getList.forEach((list) => {
+            if (list.name.toLowerCase() === name.toLowerCase()) {
+                exist = true;
+            };
+        });
+        return exist;
+    }
 }
 
 
@@ -91,10 +157,8 @@ if (!user.isset()) {
     user.create(app.generateID(format = 'xxxxxxxx'));
 }
 
-console.log(user.getId());
-
-
 app.addList(user, list);
+app.renderList(list);
 
 
 
